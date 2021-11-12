d84d2220532f4d2c5579faf15e32f21a8bc66c7d
Cristian Felipe <cfads2001@gmail.com>
2021-11-12 12:54:29 -0300

    contato objeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,13 +10,22 @@ function submitContato(){
     const email = document.getElementById("email").value;
     const telefone = document.getElementById("telefone").value;
     const mensagem = document.querySelector("textarea").value;
-    
+
+    let servicos = "";
+    const allButtons = document.querySelectorAll(".contato-servico-btn-active");
+    allButtons.forEach(botao=>{
+        servicos+=` ${botao.value}`;
+    })
+
     const enviado = {
         nome,
         email,
         telefone,
-        mensagem
+        mensagem,
+        servicos
     }
+
+    console.log(enviado);
 
     const dados = document.querySelectorAll('.isSubmit');
     dados.forEach(dado=>{
