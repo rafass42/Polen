5f6831765314957e0eb0452e985fe9245d2dbd00
Cristian Felipe <cfads2001@gmail.com>
2021-11-12 12:01:42 -0300

    contato dados js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,19 @@
 //SUBMIT CONTATOS
 function submitContato(){
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const mensagem = document.querySelector("textarea").value;
+    
+    const enviado = {
+        nome,
+        email,
+        telefone,
+        mensagem
+    }
+
+    console.log(enviado);
+
     const dados = document.querySelectorAll('.isSubmit');
     dados.forEach(dado=>{
         dado.classList.toggle("invisible");
