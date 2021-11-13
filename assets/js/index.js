f6284a7b9101e69ae4eee146e2a377610aa0c590
Cristian Felipe <cfads2001@gmail.com>
2021-11-13 09:43:11 -0300

    contato msg enviada js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,20 @@
-//BOTÕES SERVIÇOS
+//Enviado Page
+function enviado() {
+    const parametros = new URLSearchParams(location.search);
+    enviado = parametros.get('enviado');
+    
+    if(enviado==="true"){
+        const dados = document.querySelectorAll('.isSubmit');
+        dados.forEach(dado=>{
+            dado.classList.toggle("invisible");
+        })
+    }
+}
 
-function activeButton({target}){
-    target.classList.toggle("contato-servico-btn-active");
+//BOTÕES SERVIÇOS
+function activeButton(event){
+    event.preventDefault();
+    event.target.classList.toggle("contato-servico-btn-active");
 }
 
 //SUBMIT CONTATOS
@@ -10,6 +23,7 @@ function submitContato(){
     const email = document.getElementById("email").value;
     const telefone = document.getElementById("telefone").value;
     const mensagem = document.querySelector("textarea").value;
+    document.getElementById("isEnv").setAttribute('value','true');
 
     let servicos = "";
     const allButtons = document.querySelectorAll(".contato-servico-btn-active");
@@ -25,10 +39,4 @@ function submitContato(){
         servicos
     }
 
-    console.log(enviado);
-
-    const dados = document.querySelectorAll('.isSubmit');
-    dados.forEach(dado=>{
-        dado.classList.toggle("invisible");
-    })
 }
