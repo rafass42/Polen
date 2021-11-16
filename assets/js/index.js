d8ed924812fe096e661ca3b1df0dfec860b20cfe
Cristian Felipe <cfads2001@gmail.com>
2021-11-16 10:51:26 -0300

    search contato

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 //Enviado Page
 function enviado() {
     console.log("foi");
-    let link = window.location.href.includes("XlmdskI");
-    if(link){
+    let link = location.search;
+    if(link=="XlmdskI=Envl0xk"){
         const dados = document.querySelectorAll('.isSubmit');
         dados.forEach(dado=>{
             dado.classList.toggle("invisible");
