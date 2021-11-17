6abaa157f9cf1f8ce7b0e87b06452d15d3f7712f
Cris <cfads2001@gmail.com>
2021-11-17 11:54:56 -0300

    accordion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
+//Accordion Produtos
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 //Enviado Page
 function enviado() {
     let link = location.search;
